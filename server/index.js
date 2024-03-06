594c9a329bc9bb97d02a7beec098ffef27da096d
Alessandro Chiabodo <alessandro.chiabodo@gmail.com>
2024-03-06 12:32:35 +0100

    update - added tls support

--- server/index.js
+++ server/index.js
@@ -6,7 +6,15 @@ import cookieParser from "cookie-parser";
 import multer from "multer";
 import cors from "cors";
 import env from "dotenv";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
+// Load ports from the .env file
+const httpsPort = process.env.HTTPS_PORT || 1337;
+const httpPort = process.env.HTTP_PORT || 3001;
+
+// Load environment variables from the .env file
 const result = env.config();
 // Create an instance of the Express application
 const app = express();
@@ -18,14 +26,18 @@ app.use(cookieParser());
 
 app.use('/', express.static('dist'));
 
+
 // set-up the CORS middleware
 const corsOptions = {
-  origin: result.parsed.CORS_ORIGIN || 'http://localhost:3001',
-  credentials: true,
+ origin: [
+    result.parsed.CORS_ORIGIN || 'http://localhost:3001',
+    result.parsed.HTTPS_CORS_ORIGIN || 'https://localhost:1337'
+ ],
+ credentials: true,
 };
 
 app.use(cors(corsOptions));
-
+console.log('CORS_ORIGIN: ' + corsOptions.origin);
 // Configuration object for setting destination and filename for the uploaded file
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -57,7 +69,52 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/categories", categoryRoutes);
+
 // Start the server and listen on port 3001
-app.listen(3001, () => {
-  console.log("Connected...");
+app.listen(httpPort, () => {
+  console.log("Unsecure API server Listening on port " + httpPort);
 });
+
+/* Setup the TLS certificates and connection*/
+let urlConfig = {};
+urlConfig.SSL_KEY = result.parsed.SSL_KEY || './cert/key.pem';
+urlConfig.SSL_CERT = result.parsed.SSL_CERT || './cert/cert.pem';
+
+const options = {
+  key:fs.readFileSync('./cert/key.pem'),
+  cert:fs.readFileSync('./cert/cert.pem')
+  }
+const sslServer=https.createServer(options,app);
+  sslServer.listen(1337,()=>{
+    console.log('API server listening on port ' + httpsPort + ' (SSL Connection)');
+  })
+
+/*
+let urlConfig = {};
+urlConfig.SSL_KEY = "/etc/ssl/PATH/example.key";
+urlConfig.SSL_CERT = "/etc/ssl/PATH/STAR_example_com.crt";
+urlConfig.SSL_CA = "/etc/ssl/PATH/STAR_example_com.ca-bundle";var app = express();
+var fs = require('fs');
+var appPort = 5000;
+var appPortHttps = 5001; //for SSL. yes!! we can use different port for SSL other then 443.// SSL Configuration
+if (urlConfig.SSL_KEY != "") {
+ let key = fs.readFileSync(urlConfig.SSL_KEY);
+ let cert = fs.readFileSync(urlConfig.SSL_CERT);
+ let ca = fs.readFileSync(urlConfig.SSL_CA);
+let options = {
+ key: key,
+ cert: cert,
+ ca: ca
+ };
+let https = require('https').Server(options, app);
+ https.listen(appPortHttps, function () {
+ console.log('API server listening on port ' + appPortHttps + ' (SSL Connection)');
+ });
+}
+// SSL Configuration ends // Normal HTTP configuration
+let http = require('http').Server(app);
+http.listen(appPort, function() {
+ console.log('API server listening on port ' + appPort);
+});
+// Normal HTTP configuration ends
+*/
