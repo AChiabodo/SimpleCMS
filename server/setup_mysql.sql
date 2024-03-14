36116df237468581892bde713441321212e3390f
Alessandro Chiabodo <alessandro.chiabodo@gmail.com>
2024-03-15 00:20:45 +0100

    update - changed SQL setup

--- server/setup_mysql.sql
+++ server/setup_mysql.sql
@@ -1,28 +1,30 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS blog_app;
--- create table users
-CREATE TABLE IF NOT EXISTS `blog_app`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `img` VARCHAR(255) NULL,
+-- blog_app.users definition
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `role` varchar(100) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 -- create table posts
-CREATE TABLE IF NOT EXISTS `blog_app`.`posts` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) NOT NULL,
-  `desc` VARCHAR(1000) NOT NULL,
-  `img` VARCHAR(255) NOT NULL,
-  `cat` VARCHAR(255) NOT NULL,
-  `date` DATETIME NOT NULL,
-  `uid` INT NOT NULL,
+-- blog_app.posts definition
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `desc` varchar(1000) NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `cat` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `uid` int(11) NOT NULL,
+  `text` text DEFAULT NULL,
+  `draft` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
-  INDEX `uid_idx` (`uid` ASC) VISIBLE,
-  CONSTRAINT `uid`
-  FOREIGN KEY (`uid`)
-  REFERENCES `blog_app`.`users` (`id`)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
-);
+  KEY `uid_idx` (`uid`),
+  KEY `posts_FK` (`cat`),
+  CONSTRAINT `posts_FK` FOREIGN KEY (`cat`) REFERENCES `categories` (`id`),
+  CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
